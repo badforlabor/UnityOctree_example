b817abdeb0fe1dbcea808d5615d98a1b10236347
badlabor <badlabor@work.com>
2017-12-07 14:07:09 +0800

    123

---
+++ test_octree/Assets/Scripts/example/Editor/MyBoundTreeInspector.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MyBoundTree))]
+public class MyBoundTreeInspector : Editor
+{
+    public override void OnInspectorGUI()
+    {
+ 	    base.OnInspectorGUI();
+
+        if (GUILayout.Button("随机创建"))
+        {
+            MyBoundTree root = this.target as MyBoundTree;
+
+            var prefab = Resources.Load("Cube");
+            int min = -100, max = 100;
+            for (int i = 0; i < 100; i++)
+            {
+                var go = GameObject.Instantiate(prefab) as GameObject;
+                go.transform.parent = root.Cubes;
+                var v = new Vector3(Random.Range(-100, 100), Random.Range(-100, 100), Random.Range(-100, 100));
+                var s = new Vector3(Random.Range(1, 10), Random.Range(1, 10), Random.Range(1, 10));
+                go.transform.position = v;
+                go.transform.localScale = s;
+            }
+
+            EditorApplication.MarkSceneDirty();
+        }
+    }
+}
---
+++ test_octree/Assets/Scripts/example/MyBoundTree.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBoundTree : MonoBehaviour 
+{
+
+    public Transform Cubes;
+    public BoxCollider Tester;
+
+    BoundsOctree<GameObject> boundsTree = null;
+
+	// Use this for initialization
+	void Start () 
+    {
+        boundsTree = new BoundsOctree<GameObject>(15, transform.position, 1, 1.25f);
+
+        for (int i = 0; i < Cubes.childCount; i++)
+        {
+            var t = Cubes.GetChild(i);
+            boundsTree.Add(t.gameObject, t.gameObject.GetComponent<BoxCollider>().bounds);
+        }
+	}
+    void Update() 
+    {
+        if (boundsTree == null || Tester == null)
+            return;
+
+        for (int i = 0; i < Cubes.childCount; i++)
+        {
+            Cubes.GetChild(i).GetComponent<Renderer>().enabled = false;
+        }
+
+        List<GameObject> collides = new List<GameObject>();
+        boundsTree.GetColliding(collides, Tester.bounds);
+        for (int i = 0; i < collides.Count; i++)
+        {
+            collides[i].GetComponent<Renderer>().enabled = true;
+        }
+
+    }
+
+    void OnDrawGizmos()
+    {
+        if (boundsTree != null)
+        {
+            boundsTree.DrawAllBounds();
+        }
+    }
+
+}
